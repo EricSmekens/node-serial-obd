1d9fcced7c9e745733cc7fcafb8a3ff8d0f1acce
EricSmekens <ericsmekens@msn.com>
2013-03-29 11:13:26 +0100

    Added DTC and VIN tests.

--- tests/jasmine/obd.spec.js
+++ tests/jasmine/obd.spec.js
@@ -154,6 +154,46 @@ describe("node-serial-obd", function() {
 
     });
 
+    describe("DTC", function() { //Vehicle Identification number
+        it("can be read", function(){
+            dataReceivedMarker = false;
+            serialOBDReader.requestValueByName("vin");
+            waitsFor(function () {
+                return dataReceivedMarker;
+            }, "Receiving time expired", 4000);
+            runs(function() {
+                expect(dataReceivedMarker).toEqual(jasmine.any(String));
+                dataReceivedMarker = false;
+            });
+        });
+        it("can be read", function(){
+            dataReceivedMarker = false;
+            serialOBDReader.requestValueByName("vin");
+            waitsFor(function () {
+                return dataReceivedMarker;
+            }, "Receiving time expired", 4000);
+            runs(function() {
+                expect(dataReceivedMarker).toEqual(jasmine.any(String));
+                dataReceivedMarker = false;
+            });
+        });
+    });
+    
+/*  //Not supported with OBDsim.
+    describe("can read the VIN", function() { //Vehicle Identification number
+        it("can be sent", function(){
+            dataReceivedMarker = false;
+            serialOBDReader.requestValueByName("vin");
+            waitsFor(function () {
+                return dataReceivedMarker;
+            }, "Receiving time expired", 4000);
+            runs(function() {
+                expect(dataReceivedMarker).toEqual(jasmine.any(String));
+                dataReceivedMarker = false;
+            });
+        });
+    });*/
+
 
 
     it("can close the bluetooth serial port", function() {
